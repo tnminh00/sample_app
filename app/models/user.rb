ee9b05c3f8d55f30a3fdf6d826b6ff352863f57d
Tran Nhat Minh <tn.minh00@gmail.com>
2019-10-16 14:40:47 +0700

    Password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
   USER_PARAMS = %i(name email password password_confirmation).freeze
+  PASSWORD_PARAMS = %i(password password_confirmation).freeze
   validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, presence: true, length: {maximum: Settings.email.maximum}, 
     format: {with: Settings.valid_email_regex}, uniqueness:true
@@ -45,7 +46,20 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-  
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attributes reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.reset_sent_at.hours.ago
+  end
+
   private
 
     def downcase_email
