5e8674746b72037be355954deb0ab889a5e17a8b
Tran Nhat Minh <tn.minh00@gmail.com>
2019-10-17 08:12:01 +0700

    Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  USER_PARAMS = %i(name email password password_confirmation).freeze
   before_save {self.email = email.downcase}
-  validates :name, presence: true, length: {Settings.name.maximum}
-  validates :email, presence: true, length: {Settings.email.maximum}, 
-    format:{with: Settings.valid_email_regex}, uniqueness:true
-  validates :password, presence: true, length: {Settings.password.minimum}
+  validates :name, presence: true, length: {maximum: Settings.name.maximum}
+  validates :email, presence: true, length: {maximum: Settings.email.maximum}, 
+    format: {with: Settings.valid_email_regex}, uniqueness:true
+  validates :password, presence: true,
+    length: {minimum: Settings.password.minimum}
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,12 @@
-ActiveRecord::Schema.define(version: 2019_10_17_010352) do
+ActiveRecord::Schema.define(version: 2019_10_17_023745) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
